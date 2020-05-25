59e6dbf331432585a827194ea1a05fb4e592dd16
katguerin <katguerin55@gmail.com>
2020-05-24 20:32:04 -0700

    not quite sure what to do next...

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+function createNewNote(title, text) {
+    console.log(title)
+    return title;
+}
+
+
+
+
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
   });
+  
